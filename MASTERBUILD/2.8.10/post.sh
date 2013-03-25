9ec66c2e408cb9f3292abddbe99e314fb0519b79
Jupiter <jupiterh@vpac.org>
2013-03-25 11:46:07 +1100

    Add auto build list

--- MASTERBUILD/2.8.10/post.sh
+++ MASTERBUILD/2.8.10/post.sh
@@ -2,3 +2,11 @@
 
 BIN="/usr/local/bin"
 chown 755 ${BIN}/cvlbuild ${BIN}/cvlcreate
+
+SOURCE="/usr/local/src"
+BUILD_LIST="CvlBuildList.txt"
+if [ -d ${SOURCE} ]; then
+    if [ ! -f ${SOURCE}/${BUILD_LIST} ]; then
+        cp -f ${BIN}/${BUILD_LIST} ${SOURCE}
+    fi
+fi
